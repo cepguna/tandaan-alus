bfac1f37a04198f8242b5270d0a05d2c19272646
joonacode <joonacode@gmail.com>
2025-05-24 16:18:23 +0700

    chore(backend): update generated types

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -8,12 +8,16 @@
  * @module
  */
 
-import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server';
-import type * as auth from '../auth.js';
-import type * as http from '../http.js';
-import type * as resendOtp from '../resendOtp.js';
-import type * as sites from '../sites.js';
-import type * as user from '../user.js';
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as resendOtp from "../resendOtp.js";
+import type * as sites from "../sites.js";
+import type * as user from "../user.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -30,5 +34,11 @@ declare const fullApi: ApiFromModules<{
   sites: typeof sites;
   user: typeof user;
 }>;
-export declare const api: FilterApi<typeof fullApi, FunctionReference<any, 'public'>>;
-export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, 'internal'>>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
