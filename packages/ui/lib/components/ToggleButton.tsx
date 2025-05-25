c774583bcedd5b101a8b9f05b4e5bb108a27551c
joonacode <joonacode@gmail.com>
2025-05-26 01:58:22 +0700

    chore(ui): update toggle theme component

--- packages/ui/lib/components/ToggleButton.tsx
+++ packages/ui/lib/components/ToggleButton.tsx
@@ -2,24 +2,25 @@ import { exampleThemeStorage } from '@extension/storage';
 import { useStorage } from '@extension/shared';
 import type { ComponentPropsWithoutRef } from 'react';
 import { cn } from '@/lib/utils';
+import { Moon, Sun } from 'lucide-react';
 
 type ToggleButtonProps = ComponentPropsWithoutRef<'button'>;
 
-export const ToggleButton = ({ className, children, ...props }: ToggleButtonProps) => {
+export const ToggleButton = ({ className, ...props }: ToggleButtonProps) => {
   const theme = useStorage(exampleThemeStorage);
 
   return (
     <button
       className={cn(
         className,
-        'py-1 px-4 rounded shadow hover:scale-105',
+        'py-2 px-4 rounded shadow hover:scale-105',
         theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
         theme === 'light' ? 'border-black' : 'border-white',
-        'mt-4 border-2 font-bold',
+        'border font-bold',
       )}
       onClick={exampleThemeStorage.toggle}
       {...props}>
-      {children}
+      {theme === 'light' ? <Sun size={12} /> : <Moon size={12} />}
     </button>
   );
 };
